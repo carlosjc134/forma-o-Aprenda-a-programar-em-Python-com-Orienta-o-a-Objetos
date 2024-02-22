5454a2c71a8b02a5ebc306f0b84ce5b1af0d980d
josé carlos <jc_solos@hotmail.com>
2024-02-22 20:16:43 -0300

    mudando

--- 01 - python começando com a linguagem/jogos/forca.py
+++ 01 - python começando com a linguagem/jogos/forca.py
@@ -1,4 +1,12 @@
-print("*********************************")
-print("***Bem vindo no jogo de forca***!")
-print("*********************************")
 
+def jogar():
+
+    print("*********************************")
+    print("***Bem vindo no jogo de forca!***")
+    print("*********************************")
+
+    print("Fim do jogo")
+
+
+if __name__ == "__main__":
+    jogar()
