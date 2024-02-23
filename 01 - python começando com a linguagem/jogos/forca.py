bc1d58d71a8a4feb3b1b72444cdd94cefce01e38
josé carlos <jc_solos@hotmail.com>
2024-02-22 21:02:52 -0300

    mudando

--- 01 - python começando com a linguagem/jogos/forca.py
+++ 01 - python começando com a linguagem/jogos/forca.py
@@ -5,6 +5,24 @@ def jogar():
     print("***Bem vindo no jogo de forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    # enquanto True
+    while not enforcou and not acertou:
+        chute = input("Qual letra ")
+
+        index = 0
+
+        for letra in palavra_secreta:
+            if chute == letra:
+                print(f"Encontrei a letra {letra} na posição {index}")
+            index = index + 1
+
+        print("Jogando...")
+
     print("Fim do jogo")
 
 
